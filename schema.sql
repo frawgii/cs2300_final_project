17a44f752dd656657fa437526b5d03412a20ab52
sam_hajdukiewicz <samanthahajdukiewicz@gmail.com>
2025-04-30 17:51:59 -0500

    Updated schema and changed main page

--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
 -- Dropping tables in order if they exist to keep database consistent to project
-DROP TABLE IF EXISTS character_appearances;
 DROP TABLE IF EXISTS media_has_character;
 DROP TABLE IF EXISTS crossover;
 DROP TABLE IF EXISTS has_background;
@@ -21,6 +20,7 @@ DROP TABLE IF EXISTS interacted_with;
 DROP TABLE IF EXISTS users;
 
 
+-- Creating tables for the database
 CREATE TABLE users (
     username TEXT PRIMARY KEY,
     passwd TEXT NOT NULL,
@@ -40,9 +40,9 @@ CREATE TABLE song (
 );
 
 CREATE TABLE song_singers (
-    song_title TEXT,
+    s_id INT,
     s_singer TEXT NOT NULL,
-    FOREIGN KEY (song_title) REFERENCES song(s_title)
+    FOREIGN KEY (s_id) REFERENCES song(id) ON DELETE CASCADE
 );
 
 CREATE TABLE game (
@@ -55,16 +55,16 @@ CREATE TABLE game (
 );
 
 CREATE TABLE heard_in (
-    song_title TEXT,
-    game_title TEXT,
-    FOREIGN KEY (song_title) REFERENCES song(s_title),
-    FOREIGN KEY (game_title) REFERENCES game(g_title)
+    s_id INT,
+    g_id INT,
+    FOREIGN KEY (s_id) REFERENCES song(id) ON DELETE CASCADE,
+    FOREIGN KEY (g_id) REFERENCES game(id) ON DELETE CASCADE
 );
 
 CREATE TABLE game_genres (
-    game_title TEXT,
+    g_id INT,
     g_genre TEXT NOT NULL,
-    FOREIGN KEY (game_title) REFERENCES game(g_title)
+    FOREIGN KEY (g_id) REFERENCES game(id) ON DELETE CASCADE
 );
 
 CREATE TABLE comic (
@@ -78,9 +78,9 @@ CREATE TABLE comic (
 );
 
 CREATE TABLE comic_artists (
-    comic_title TEXT,
+    c_id INT,
     c_artist TEXT NOT NULL,
-    FOREIGN KEY (comic_title) REFERENCES comic(c_title)
+    FOREIGN KEY (c_id) REFERENCES comic(id) ON DELETE CASCADE
 );
 
 CREATE TABLE tv_show (
@@ -96,15 +96,15 @@ CREATE TABLE tv_show (
 );
 
 CREATE TABLE cgi_actors (
-    cgi_title TEXT,
+    tv_id INT,
     cgi_actor TEXT NOT NULL,
-    FOREIGN KEY (cgi_title) REFERENCES tv_show(tv_title)
+    FOREIGN KEY (tv_id) REFERENCES tv_show(id) ON DELETE CASCADE
 );
 
 CREATE TABLE anim_animators (
-    anim_title TEXT,
+    tv_id INT,
     anim_animator TEXT NOT NULL,
-    FOREIGN KEY (anim_title) REFERENCES tv_show(tv_title)
+    FOREIGN KEY (tv_id) REFERENCES tv_show(id) ON DELETE CASCADE
 );
 
 CREATE TABLE movie (
@@ -114,90 +114,81 @@ CREATE TABLE movie (
     favorite BOOLEAN DEFAULT 0,
     star_amount FLOAT(24) DEFAULT 0,
     media_comment TEXT,
-    mo_director TEXT,
-    FOREIGN KEY (mo_title) REFERENCES media(title)
+    mo_director TEXT
 );
 
 CREATE TABLE movie_actors (
-    mo_title TEXT,
+    mo_id INT,
     mo_actor TEXT NOT NULL,
-    FOREIGN KEY (mo_title) REFERENCES movie(mo_title)
+    FOREIGN KEY (mo_id) REFERENCES movie(id) ON DELETE CASCADE
 );
 
 CREATE TABLE interacted_with (
     u_username TEXT NOT NULL,
-    movie_title TEXT,
-    comic_title TEXT,
-    song_title TEXT,
-    tv_title TEXT,
-    game_title TEXT,
-    FOREIGN KEY (u_username) REFERENCES users(username),
-    FOREIGN KEY (movie_title) REFERENCES movie(mo_title),
-    FOREIGN KEY (comic_title) REFERENCES comic(c_title),
-    FOREIGN KEY (song_title) REFERENCES song(s_title),
-    FOREIGN KEY (tv_title) REFERENCES tv_show(tv_title),
-    FOREIGN KEY (game_title) REFERENCES game(g_title)
+    mo_id INT,
+    c_id INT,
+    s_id INT,
+    g_id INT,
+    tv_id INT,
+    date_of_interaction TEXT NOT NULL,
+    FOREIGN KEY (u_username) REFERENCES users(username) ON DELETE CASCADE,
+    FOREIGN KEY (mo_id) REFERENCES movie(id) ON DELETE CASCADE,
+    FOREIGN KEY (c_id) REFERENCES comic(id) ON DELETE CASCADE,
+    FOREIGN KEY (s_id) REFERENCES song(id) ON DELETE CASCADE,
+    FOREIGN KEY (g_id) REFERENCES game(id) ON DELETE CASCADE,
+    FOREIGN KEY (tv_id) REFERENCES tv_show(id) ON DELETE CASCADE
 );
 
 CREATE TABLE media_character (
-    ch_name TEXT PRIMARY KEY,
+    ch_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ch_name TEXT UNIQUE NOT NULL,
     species TEXT NOT NULL,
-    skin_color TEXT,
+    fur_color TEXT,
     eye_color TEXT,
-    fur_color TEXT NOT NULL
+    fluff_color TEXT NOT NULL
 );
 
 CREATE TABLE character_info (
-    char_name TEXT,
+    char_id INT,
     journal_entry TEXT,
     abilities TEXT NOT NULL,
-    FOREIGN KEY (char_name) REFERENCES media_character(ch_name)
-);
-
-CREATE TABLE character_appearances (
-    character_name TEXT,
-    appearance TEXT NOT NULL,
-    FOREIGN KEY (character_name) REFERENCES character_info(char_name)
+    appeaarance TEXT,
+    FOREIGN KEY (char_id) REFERENCES media_character(ch_id) ON DELETE CASCADE
 );
 
 CREATE TABLE has_background (
-    char_name TEXT,
-    ch_info TEXT,
-    FOREIGN KEY (char_name) REFERENCES media_character(ch_name),
-    FOREIGN KEY (ch_info) REFERENCES character_info(char_name)
+    char_id INT,
+    ch_info_id INT,
+    FOREIGN KEY (char_id) REFERENCES media_character(ch_id) ON DELETE CASCADE,
+    FOREIGN KEY (ch_info_id) REFERENCES character_info(char_id) ON DELETE CASCADE
 );
 
 CREATE TABLE crossover (
-    game_title TEXT,
-    comic_title TEXT,
-    character_name TEXT,
-    FOREIGN KEY (game_title) REFERENCES game(g_title),
-    FOREIGN KEY (comic_title) REFERENCES comic(c_title),
-    FOREIGN KEY (Character_name) REFERENCES media_character(ch_name)
+    g_id INT,
+    c_id INT,
+    char_id INT,
+    FOREIGN KEY (g_id) REFERENCES game(id) ON DELETE CASCADE,
+    FOREIGN KEY (c_id) REFERENCES comic(id) ON DELETE CASCADE,
+    FOREIGN KEY (char_id) REFERENCES media_character(ch_id) ON DELETE CASCADE
 );
 
 CREATE TABLE media_has_character (
-    character_name TEXT NOT NULL,
-    game_title TEXT,
-    comic_title TEXT,
-    tv_title TEXT,
-    movie_title TEXT,
-    song_title TEXT,
-    FOREIGN KEY (character_name) REFERENCES media_character(ch_name),
-    FOREIGN KEY (game_title) REFERENCES game(g_title),
-    FOREIGN KEY (comic_title) REFERENCES comic(c_title),
-    FOREIGN KEY (tv_title) REFERENCES tv_show(tv_title),
-    FOREIGN KEY (movie_title) REFERENCES movie(mo_title),
-    FOREIGN KEY (song_title) REFERENCES song(s_title)
+    ch_id INT NOT NULL,
+    g_id INT,
+    c_id INT,
+    tv_id INT,
+    mo_id INT,
+    s_id INT,
+    FOREIGN KEY (ch_id) REFERENCES media_character(ch_id) ON DELETE CASCADE,
+    FOREIGN KEY (g_id) REFERENCES game(id) ON DELETE CASCADE,
+    FOREIGN KEY (c_id) REFERENCES comic(id) ON DELETE CASCADE,
+    FOREIGN KEY (tv_id) REFERENCES tv_show(id) ON DELETE CASCADE,
+    FOREIGN KEY (mo_id) REFERENCES movie(id) ON DELETE CASCADE,
+    FOREIGN KEY (s_id) REFERENCES song(id) ON DELETE CASCADE
 );
 
 
 -- Insertion for initial table values
-INSERT INTO posts (title, content)
-VALUES 
-   ("First Post", "This is my first post"),
-   ("Second Post", "This is my second post");
-
 INSERT INTO users (username, passwd, email, phone_no)
 VALUES
     ("sam_haj", "ilovecs2300", "sam@mst.edu", 3145551234);
@@ -230,3 +221,110 @@ VALUES
     ("Sonic the Hedgehog", 2020, true, 4.8, "Great adaptation", "Jeff Fowler"),
     ("Sonic the Hedgehog 2", 2022, true, 4.9, "Even better than the first", "Jeff Fowler"),
     ("Sonic the Hedgehog 3", 2024, false, 3.5, "Disappointing sequel", "Jeff Fowler");
+
+INSERT INTO media_character (ch_name, species, fur_color, eye_color, fluff_color)
+VALUES
+    ("Sonic", "Hedgehog", "Blue", "Green", "Tan"),
+    ("Tails", "Fox", "Orange", "Green", "White"),
+    ("Knuckles", "Echidna", "Red", "Green", "White"),
+    ("Amy Rose", "Hedgehog", "Pink", "Green", "Pink"),
+    ("Shadow", "Hedgehog", "Black", "Red", "White");
+
+INSERT INTO song_singers (s_id, s_singer)
+VALUES
+    (1, "Kellin Quinn"),
+    (1, "Sonic Team"),
+    (2, "Sonic Forces Soundtrack"),
+    (3, "Crush 40");
+
+INSERT INTO heard_in(s_id, g_id)
+VALUES
+    (2, 2);
+
+INSERT INTO game_genres(g_id, g_genre)
+VALUES
+    (1, "Platformer"),
+    (1, "3D"),
+    (2, "Platformer"),
+    (3, "Platformer"),
+    (3, "Beat 'Em Up");
+
+INSERT INTO comic_artists(c_id, c_artist)
+VALUES
+    (1, "Ian Flynn"),
+    (2, "Evan Stanley");
+
+INSERT INTO cgi_actors(tv_id, cgi_actor)
+VALUES
+    (3, "Ben Schwartz"),
+    (3, "Colleen O'Shaughnessey"),
+    (3, "Idris Elba");
+
+INSERT INTO anim_animators(tv_id, anim_animator)
+VALUES
+    (1, "Dee Bradley Baker"),
+    (1, "Mike Pollock"),
+    (2, "Jason Griffith"),
+    (2, "Dan Green");
+
+INSERT INTO movie_actors(mo_id, mo_actor)
+VALUES
+    (1, "Ben Schwartz"),
+    (1, "James Marsden"),
+    (2, "Jim Carrey"),
+    (2, "Idris Elba"),
+    (3, "Ben Schwartz"),
+    (3, "James Marsden");
+
+INSERT INTO interacted_with(u_username, mo_id, date_of_interaction)
+VALUES
+    ("sam_haj", 1, "2023-10-01"),
+    ("sam_haj", 2, "2023-10-02"),
+    ("sam_haj", 3, "2023-10-03");
+
+INSERT INTO interacted_with(u_username, c_id, date_of_interaction)
+VALUES
+    ("sam_haj", 1, "2023-10-04"),
+    ("sam_haj", 2, "2023-10-05");
+
+INSERT INTO character_info(char_id, journal_entry, abilities, appearance)
+VALUES
+    (1, "I am the fastest thing alive!", "Super Speed", "Regular sized, blue downturned quills"),
+    (2, "I am Sonic's best friend", "Flight", "Short, two tails, white fur tuft on chest"),
+    (3, "I am the guardian of the Master Emerald", "Super Strength", "Muscular, red spiked quills"),
+    (4, "I am Sonic's girlfriend", "Hammer Attack", "Short pink spikes, red dress"),
+    (5, "I am the ultimate lifeform", "Chaos Control", "Red stripes on arms and quills");
+
+INSERT INTO has_background(char_id, ch_info_id)
+VALUES
+    (1, 1),
+    (2, 2),
+    (3, 3),
+    (4, 4),
+    (5, 5);
+
+INSERT INTO crossover(g_id, c_id, char_id)
+VALUES
+    (1, 1, 1),
+    (2, 2, 2),
+    (3, 1, 3),
+    (1, 2, 4),
+    (2, 3, 5);
+
+INSERT INTO media_has_character(ch_id, g_id)
+VALUES
+    (1, 1),
+    (1, 2),
+    (1, 3),
+    (2, 1),
+    (2, 2),
+    (2, 3),
+    (3, 1),
+    (3, 2),
+    (3, 3),
+    (4, 1),
+    (4, 3),
+    (5, 1),
+    (5, 3);
+
+
